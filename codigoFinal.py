9968ccc4b9d508290ef14aba6955ccafefc3cf5f
Vinícyus Brasil <vinicyusbrasil1@hotmail.com>
2020-03-23 18:59:30 -0300

    Add files via upload

--- codigoFinal.py
+++ codigoFinal.py
@@ -5,61 +5,91 @@ Created on Sat Mar 21 17:33:02 2020
 @author: N13M4ND
 """
 
-from sklearn import linear_model
-from sklearn import datasets
+
 import pandas as pd
 import numpy as np
-import statsmodels.api as sm
-import statsmodels.formula.api as smf
+from sklearn.model_selection import train_test_split
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import FeatureUnion
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 
-dados = pd.read_csv("dados/escore_treino.csv")
-datas = dados.dropna()
+class DataFrameSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, attribute_names):
+        self.attribute_names = attribute_names
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X):
+        return X[self.attribute_names].values
+    
+class MyLabelBinarizer(TransformerMixin):
+    def __init__(self, *args, **kwargs):
+        self.encoder = LabelBinarizer(*args, **kwargs)
+    def fit(self, x, y=0):
+        self.encoder.fit(x)
+        return self
+    def transform(self, x, y=0):
+        return self.encoder.transform(x)
+def limpar_dados(data):
+    data = data.dropna()
+    return data
 
-z = datas['salario'].values
-x = datas['experiência'].values
-w = datas['idade'].values
-e = datas['escore'].values
-xlog = np.log(x + 1)
-zlog = np.log(z + 1)
+def divisor(data):
+    train_set, test_set = train_test_split(data, test_size=0.2, random_state=42)  
+    return train_set, test_set
 
-numericas = datas.select_dtypes('number')
-categoricas = datas.select_dtypes('object')
+def arrumador_treinos(db):
+    DADOS_SEMLABELS = db.drop('escore', axis=1)
+    DADOS_LABELS = db['escore'].copy()
+    return DADOS_SEMLABELS, DADOS_LABELS
 
-categoricas = pd.get_dummies(categoricas[categoricas.columns])
+def arrumador_categoricas(db):
+    CATEGORICAS = ['grau_escolaridade']
+    x = db.drop('estado_civil', axis=1)
+    numericas = x.drop('grau_escolaridade',axis=1)
+    print(numericas.info())
+    num_pipeline = Pipeline([('selector', DataFrameSelector(list(numericas))),
+                            ('std_scaler', StandardScaler())])
+    
+    cat_pipeline = Pipeline([('selector', DataFrameSelector(CATEGORICAS)),
+                            ('label_binarizer', MyLabelBinarizer())])
+    
+    full_pipeline = FeatureUnion(transformer_list=[
+    ("num_pipeline", num_pipeline),
+    ("cat_pipeline", cat_pipeline),
+    ])
+    
+    DADOS_PREPARADOS = full_pipeline.fit_transform(x)
+    return DADOS_PREPARADOS
 
-feat = pd.concat([numericas, categoricas], axis=1)
+def criaModelo(y, labels):
+    return LinearRegression().fit(y, labels)
+    
+    
+def main():
+    DADOS_TREINO = pd.read_csv("dados/escore_treino.csv")
+    DADOS_SEMNA = limpar_dados(DADOS_TREINO)
+    RESPOSTA = input("É a fase de treino?\n")
+    
+    if RESPOSTA == "y":
+        print("Fase de treinamento iniciada.")
+        TREINO, TESTE = divisor(DADOS_SEMNA)   
+        DADOS_SEMLABELS, DADOS_LABELS = arrumador_treinos(TREINO)
+        DADOS_SEMLABELS_TESTE, DADOS_LABELS_TESTE = arrumador_treinos(TESTE)
+        DADOS_PRONTOS = arrumador_categoricas(DADOS_SEMLABELS)
+        print(DADOS_PRONTOS.shape) #tirei o estado civil
+        model = criaModelo(DADOS_PRONTOS, DADOS_LABELS)
+        yhat = model.predict(DADOS_PRONTOS)
+        print(r2_score(DADOS_LABELS, yhat))
+    else:
+        print("Vejo que você está testando, então. ")
+    
+    
 
-feat.to_csv('features.csv')
+if __name__ == "__main__":
+    main()
 
-print(list(feat))
 
-e1 = feat['grau_escolaridade_fund. completo'].values
-e2 = feat['grau_escolaridade_fund. incompleto'].values
-e3 = feat['grau_escolaridade_médio completo'].values
-e4 = feat['grau_escolaridade_médio incompleto'].values
-e5 = feat['grau_escolaridade_pós-graduação'].values
-e6 = feat['grau_escolaridade_superior completo'].values
-e7 = feat['grau_escolaridade_superior incompleto'].values
-s1 = feat['estado_civil_casado'].values
-s2 = feat['estado_civil_divorciado'].values
-s3 = feat['estado_civil_separado'].values
-s4 = feat['estado_civil_solteiro'].values
-s5 = feat['estado_civil_união estável formal'].values
-s6 = feat['estado_civil_união estável informal'].values
-s7 = feat['estado_civil_viúvo'].values
-
-mod = smf.ols('e ~ zlog + w + xlog + e1 + e2 + e3 + e4 + e5 + e6 + e7 + s1 + s2 + s3 + s4 + s5 + s6 + s7', data=feat)
-resultados = mod.fit()
-print(resultados.summary())
-
-teste = pd.read_csv("dados/escore_teste.csv")
-
-numericas = teste.select_dtypes('number')
-categoricas2 = teste.select_dtypes('object')
-categoricas2 = pd.get_dummies(categoricas2[categoricas2.columns])
-features2 = pd.concat([numericas, categoricas2], axis=1)
-features2.to_csv('features2.csv')
-features2['escore'] = ''
-print(type(features2))
-predito = resultados.predict(sm.add_constant(features2['escore'].values), transform=False)
-print(predito)

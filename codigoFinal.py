82ec19edf0757d4956efbac802e25b78f19568ea
Vinícyus Brasil <vinicyusbrasil1@hotmail.com>
2020-03-23 21:27:53 -0300

    Add files via upload

--- codigoFinal.py
+++ codigoFinal.py
@@ -16,6 +16,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import FeatureUnion
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
+from sklearn.impute import SimpleImputer
 
 class DataFrameSelector(BaseEstimator, TransformerMixin):
     def __init__(self, attribute_names):
@@ -52,6 +53,7 @@ def arrumador_categoricas(db):
     numericas = x.drop('grau_escolaridade',axis=1)
     print(numericas.info())
     num_pipeline = Pipeline([('selector', DataFrameSelector(list(numericas))),
+                             ('imputer', SimpleImputer(strategy='median')),
                             ('std_scaler', StandardScaler())])
     
     cat_pipeline = Pipeline([('selector', DataFrameSelector(CATEGORICAS)),
@@ -62,21 +64,32 @@ def arrumador_categoricas(db):
     ("cat_pipeline", cat_pipeline),
     ])
     
-    DADOS_PREPARADOS = full_pipeline.fit_transform(x)
+    DADOS_PREPARADOS = full_pipeline.fit_transform(x.astype(str))
     return DADOS_PREPARADOS
 
 def criaModelo(y, labels):
     return LinearRegression().fit(y, labels)
 def testar(modelo, dadostestaveis):  
     return modelo.predict(dadostestaveis)
+
+def enchermedia(dados, table):
+    x = dados[table].mean()
+    dados[table] = dados[table].fillna(x)
+    return dados
+
+def encherEscolaridade(dados, table):
+    dados[table] = dados[table].fillna('pós-graduação')
+    return dados
 def main():
     DADOS_TREINO = pd.read_csv("dados/escore_treino.csv")
     DADOS_SEMNA = limpar_dados(DADOS_TREINO)
-  
+    print(DADOS_SEMNA.shape)
     print("Fase de treinamento iniciada.")
     
     TREINO, TESTE = divisor(DADOS_SEMNA)   
     DADOS_SEMLABELS, DADOS_LABELS = arrumador_treinos(TREINO)
+    print(DADOS_SEMLABELS.info())
+    #print(DADOS_SEMLABELS['grau_escolaridade'].value_counts())
     DADOS_SEMLABELS_TESTE, DADOS_LABELS_TESTE = arrumador_treinos(TESTE)
     DADOS_PRONTOS = arrumador_categoricas(DADOS_SEMLABELS)
     print(DADOS_PRONTOS.shape) #tirei o estado civil
@@ -89,12 +102,26 @@ def main():
     
     DADOS_TRAB = pd.read_csv("dados/escore_teste.csv")
     #print(DADOS_TRAB.describe())
-    DADOS_TRAB_SEMNA = limpar_dados(DADOS_TRAB)
-    #print(DADOS_TRAB_SEMNA.describe())
-    DADOS_TRAB_PRONTO = arrumador_categoricas(DADOS_TRAB_SEMNA)
+    DADOS_TRAB_SEMNA = enchermedia(DADOS_TRAB, 'experiência')
+    DADOS_TRAB_SEMNA_ENCHIDO = encherEscolaridade(DADOS_TRAB_SEMNA, 'grau_escolaridade')
+    print(DADOS_TRAB.info())
+    #print(DADOS_TRAB_SEMNA['grau_escolaridade'].value_counts())
+    DADOS_TRAB_PRONTO = arrumador_categoricas(DADOS_TRAB_SEMNA_ENCHIDO)
+    #print(DADOS_TRAB_PRONTO.describe())
+    print(DADOS_PRONTOS.shape)
+    df = pd.DataFrame(data=DADOS_PRONTOS)
+    df.to_csv("DADOS_PRONTOS.csv")
+    df = pd.DataFrame(data=DADOS_TRAB_PRONTO)
+    df.to_csv("DADOS_TRAB_PRONTOS.csv")
+    print(DADOS_TRAB_PRONTO.shape)
     yhattrab = testar(model, DADOS_TRAB_PRONTO)
     print(yhattrab)
+    df = pd.DataFrame(data=yhattrab, index=np.arange(3000,4000))
+    df.columns=['escore']
+    df.to_csv("final.csv")
+    
 if __name__ == "__main__":
     main()
 
 
+#por algum motivo, sempre tem que colocar o Id no csv
